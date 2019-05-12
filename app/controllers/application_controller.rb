160d4210dab77014efdecf567ec94dc1208a65bf
Dan Hintz <dhintz@live.com>
2019-05-12 21:23:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,11 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
     @user = User.new(username: params[:username], password: params[:password])
-    
+    if @user.save
+      redirect '/login'
+    else
+      redirect '/failure'
+    end
   end
 
   get '/account' do
@@ -34,6 +38,8 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
   end
 
   get "/failure" do
